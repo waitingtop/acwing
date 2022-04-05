d2cab353cec119a31587383e0dcd8e00592246ea
beichuan <87553207+waitingtop@users.noreply.github.com>
2022-04-05 22:33:53 +0800

    799. 最长连续不重复子序列

---
+++ longestSubsequence.c
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+const int N = 1e5 + 10;
+int n;
+int a[N], s[N];
+
+int main()
+{
+    cin >> n;
+    for(int i = 0; i < n; ++i) cin >> a[i];
+    
+    int res = 0;
+    for(int i = 0, j = 0; i < n; ++i)
+    {
+        s[a[i]]++;
+        while(s[a[i]] > 1)
+        {
+            s[a[j]]--;
+            ++j;
+        }
+        res = max(res, i - j + 1);
+    }
+    cout << res << endl;
+    return 0;
+}
